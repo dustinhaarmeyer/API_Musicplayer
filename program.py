ac437709d4884e2bd82c9ef0f45af36aa6331b11
dustinhaarmeyer <81471824+dustinhaarmeyer@users.noreply.github.com>
2021-03-27 13:39:01 +0100

    Add files via upload
    First public version. Includes:
    - Control API on Port 5000
    - Folder as Playlist
    - Multiprocessing for the next step

---
+++ program.py
@@ -0,0 +1,103 @@
+#pip3 install pygame
+#pip install pygame     -> So we can use it on every python-version
+#same for flask 
+import os 
+import pygame 
+import time
+from time import sleep
+import flask
+from flask import redirect
+import multiprocessing
+
+app = flask.Flask(__name__)
+#app.config["DEBUG"] = True
+
+actual = 0
+plN = 0
+a = 0
+
+musicDir = '/test/'
+os.chdir(musicDir)
+playlist = os.listdir()
+
+print (playlist)
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load(playlist[actual])
+
+for numbers in playlist:
+    plN += 1
+print(plN)
+
+@app.route('/', methods=['GET'])
+def home():
+    return '<a href="play">->Play</a> <br> <a href="pause">->Pause</a> <br> <a href="resume">->Resume</a> <br> <a href="next">->Next</a> <br> <a href="back">->Back</a> <br> <a href="stop">->Stop</a>'
+
+@app.route('/play', methods=['GET'])
+def play():
+    pygame.mixer.music.play()
+    print('Play')
+    return redirect("/")    #"<h1>Start Playing now</p>"
+
+@app.route('/resume', methods=['GET'])
+def resume():
+    pygame.mixer.music.unpause()
+    print('Resume')
+    return redirect("/")
+
+@app.route('/pause', methods=['GET'])
+def pause():
+    pygame.mixer.music.pause()
+    return redirect("/")
+
+@app.route('/next', methods=['GET'])
+def next():
+    stop()
+    global actual
+    actual += 1
+    if actual >= plN:
+        actual = 0
+    pygame.mixer.music.load(playlist[actual])
+    sleep(1)
+    play()
+    print('Playing next')
+    return redirect("/")
+
+@app.route('/back', methods=['GET'])
+def before():
+    global actual
+    actual -= 1
+    pygame.mixer.music.load(playlist[actual])
+    stop()
+    play()
+    print('Going back')
+    return redirect("/")
+
+@app.route('/back/get_state/busy', methods=['GET'])
+def busy():
+    #print(pygame.mixer.music.get_busy())
+    return pygame.mixer.music.get_busy()
+
+@app.route('/stop', methods=['GET'])
+def stop():
+    pygame.mixer.music.stop()
+    return redirect("/")
+
+def API(Conf):
+   print('In API selction')
+   app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+    config = {"Something":"SomethingElese"}
+    p = multiprocessing.Process(target=API, args=(config))
+    p.start()
+    sleep(3)
+    print('After Flask run')
+    while busy()==False:
+            sleep(0.1)
+    sleep(1)
+    while True:
+        while busy():
+            sleep(0.1)
+        sleep(1)
+        next()
